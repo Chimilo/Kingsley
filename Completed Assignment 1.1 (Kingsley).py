b5e603ea1dc086ed4a1d90f95cdd46fa00f18b24
Chimilo <42252867+Chimilo@users.noreply.github.com>
2018-08-14 06:03:26 +0100

    Add files via upload

---
+++ Completed Assignment 1.1 (Kingsley).py
@@ -0,0 +1,17 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Enter the value of a
+: 10
+Enter the value of b
+: 12
+Enter the value of c
+: 15
+2940.0
+>>> ================================ RESTART ================================
+>>> 
+What is your name: Kingsley N. Chimilo
+How old are you: 20
+Kingsley N. Chimilo will be 100 years old in the year 2098
+>>> 
---
+++ My Assignment 1.0 (Kingsley).py
@@ -0,0 +1,12 @@
+a = float(input('Enter the value of a\n: ')) 
+b = float(input('Enter the value of b\n: '))
+c = float(input('Enter the value of c\n: '))
+
+# To take cofficient input from the users
+# a = float(input('Enter a: '))
+# b = float(input('Enter b: '))
+# c = float(input('Enter c: '))
+
+# calculate the discriminant
+d = (-b + 4 * a * c) / 2 * a
+print(d)
